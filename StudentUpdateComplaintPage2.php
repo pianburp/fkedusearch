b6abe3869282ae9ee035bcd9ee3ebebbae20216b
kelvin2321 <kelvinho6142002@gmail.com>
2023-06-21 23:34:40 +0800

    Revert "Revert "Merge branch 'complaint'""
    This reverts commit b38a57b796142a725ca44016736dbe8d0748db13.

---
+++ StudentUpdateComplaintPage2.php
@@ -0,0 +1,19 @@
+<?php
+//Connect to the database server.
+$link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+
+//Select the database.
+mysqli_select_db($link, "fkedu") or die(mysqli_error($link));
+
+    $Ctype = $_POST["Kat"];
+    $typearr= implode(',' ,$Ctype);
+    $Cdate = $_POST["tarikh"];
+    $Udesc = $_POST["content"];
+	$pid2 = $_POST["id2"];
+
+$query = "UPDATE complaint SET complaint_type = '$typearr', complaint_date = '(NOW())', complaint_desc = '$Udesc' WHERE complaintID = '$pid2'";
+
+$result = mysqli_query($link,$query) or die ("Could not execute query in ubah.php");
+if($result){
+ echo "<script type = 'text/javascript'> window.location='StudentComplaintMainPage.php' </script>";
+}
